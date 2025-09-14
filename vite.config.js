c94f24dd437ae0fe6a2658c8f7317266a2fbae1b
Семён Крекотун <97591773+SemenKr@users.noreply.github.com>
2025-09-15 00:12:49 +0300

    add sprite plugin

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path from "path"; // ✅ добавить импорт
+export default defineConfig({
+    plugins: [
+        react({
+            babel: {
+                plugins: ["babel-plugin-styled-components"],
+            },
+        }),
+        createSvgIconsPlugin({
+            iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+            symbolId: "icon-[name]",
+        }),
+    ],
+    resolve: {
+        alias: {
+            "@": "/src",
+        },
+    },
+});
